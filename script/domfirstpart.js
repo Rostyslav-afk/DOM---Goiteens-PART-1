21c3550715a9cf6ad68c3a00d2303c2ad3a18e1b
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-05-26 18:30:18 +0200

    full1

---
+++ script/domfirstpart.js
@@ -0,0 +1,33 @@
+// const headerElement = document.querySelector("h1");
+
+// const sectionElement = document.querySelector(".dom-tree");
+// console.log(sectionElement);
+
+// const imgElemnt = document.querySelector('[src="./img/dom.png"]');
+// console.log(imgElemnt);
+
+// const listElement = document.querySelector("ul");
+
+// const listChildrens = listElement.children;
+
+// const firstChildren = listElement.firstElementChild;
+
+// const titleLElement = firstChildren.firstElementChild;
+
+// const textElement = firstChildren.lastElementChild;
+
+// console.log(textElement);
+// console.log(listChildrens);
+// console.log(listElement);
+
+// const byId = document.getElementById("dom");
+// console.log(byId);
+
+// const titleElement = document.querySelector("h1");
+// titleElement.textContent = "Ало хакери? та та взломи";
+// console.log(titleElement);
+
+// const titleElement = document.querySelector("h2");
+// titleElement.style.color = "blue";
+
+const divElement = document.querySelector
