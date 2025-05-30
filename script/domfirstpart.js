65b9a87c20e8a5533e4e57e09daf4b7a203054af
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-05-30 19:44:14 +0200

    full

--- script/domfirstpart.js
+++ script/domfirstpart.js
@@ -1,33 +1,47 @@
-// const headerElement = document.querySelector("h1");
+// // const headerElement = document.querySelector("h1");
 
-// const sectionElement = document.querySelector(".dom-tree");
-// console.log(sectionElement);
+// // const sectionElement = document.querySelector(".dom-tree");
+// // console.log(sectionElement);
 
-// const imgElemnt = document.querySelector('[src="./img/dom.png"]');
-// console.log(imgElemnt);
+// // const imgElemnt = document.querySelector('[src="./img/dom.png"]');
+// // console.log(imgElemnt);
 
-// const listElement = document.querySelector("ul");
+// // const listElement = document.querySelector("ul");
 
-// const listChildrens = listElement.children;
+// // const listChildrens = listElement.children;
 
-// const firstChildren = listElement.firstElementChild;
+// // const firstChildren = listElement.firstElementChild;
 
-// const titleLElement = firstChildren.firstElementChild;
+// // const titleLElement = firstChildren.firstElementChild;
 
-// const textElement = firstChildren.lastElementChild;
+// // const textElement = firstChildren.lastElementChild;
 
-// console.log(textElement);
-// console.log(listChildrens);
-// console.log(listElement);
+// // console.log(textElement);
+// // console.log(listChildrens);
+// // console.log(listElement);
 
-// const byId = document.getElementById("dom");
-// console.log(byId);
+// // const byId = document.getElementById("dom");
+// // console.log(byId);
 
-// const titleElement = document.querySelector("h1");
-// titleElement.textContent = "Ало хакери? та та взломи";
-// console.log(titleElement);
+// // const titleElement = document.querySelector("h1");
+// // titleElement.textContent = "Ало хакери? та та взломи";
+// // console.log(titleElement);
 
-// const titleElement = document.querySelector("h2");
-// titleElement.style.color = "blue";
+// // const titleElement = document.querySelector("h2");
+// // titleElement.style.color = "blue";
 
-const divElement = document.querySelector
+// const titleE1 = document.querySelector("#dom");
+// const titleE2 = document.querySelector("#bom");
+// const inputEl = document.querySelector('[type="text"]');
+
+// titleE1.classList.add("title")
+// titleE1.replaceChild
+// console.log(inputEl);
+
+
+//#1але ше інпут добавити
+const button = document.querySelector("#button");
+button.textContent = "Click";
+console.log(button);
+
+//#3
